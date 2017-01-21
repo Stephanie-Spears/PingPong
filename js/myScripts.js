f01efbc2df189046ae8849d284de044ee17fc4d0
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-21 02:01:17 -0800

    add styling

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,35 +1,50 @@
-// function othername() {
-//   var input = document.getElementById("userInput").value;
-//   alert(input);
-// }
 
-// function getInput() {
-//   var num1 = document.getElementById("num1").value;
-//   return num1;
-// }
+function getFormInput(form) {
+  var getNum=document.myPingPongForm.inputField1.value;
 
-// function validateForm() {
-//     var num1 = document.forms["myForm"]["fname"].value; //document-html, forms[index(gets the id of the first "form" element in a document)]
-//     if (num1 == "") {
-//         alert("This section must be filled out");
-//         return false;
-//     }
-//
-// }
-function myFunction() {
-    var x, text;
+  var boolRunCode = true;
+  var num1;
+  var cleanInput;
+  var myArray = [];
 
-    // Get the value of the input field with id="numb"
-    x = document.getElementById("numb").value;
+  while (boolRunCode === true) {
+    num1 = getInput();
+    cleanInput = checkInput(num1); // NOTE: num1 is the Argument--the actual value that's being passed to the function
 
-    // If x is Not a Number or less than one or greater than 10
-    if (isNaN(x) || x < 1 || x > 10) {
-        text = "Input not valid";
-    } else {
-        text = "Input OK";
+    while (cleanInput !== true) {
+      num1 = getInput();
+      cleanInput = checkInput(num1);
     }
-    document.getElementById("demo").innerHTML = text;
+    myArray = initializeArray(num1);
+    document.myPingPongForm.append(myArray);
+    alert("MY ARRAY:" + myArray);
+    boolRunCode = false;
+  }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 var getInput = function() {
@@ -54,26 +69,21 @@ var initializeArray = function (endNum) {
   return myArray;
 };
 
-// NOTE forgot to do the three functions!
-//Numbers divisible by 3 are replaced with "ping"
-//Numbers divisible by 5 are replaced with "pong"
-//Numbers divisible by 15 are replaced with "ping-pong"
 
-
-var boolRunCode = true;
-var num1;
-var cleanInput;
-var myArray = [];
-
-while (boolRunCode === true) {
-  num1 = getInput();
-  cleanInput = checkInput(num1); // NOTE: num1 is the Argument--the actual value that's being passed to the function
-
-  while (cleanInput !== true) {
-    num1 = getInput();
-    cleanInput = checkInput(num1);
-  }
-  myArray = initializeArray(num1);
-  alert("MY ARRAY:" + myArray);
-  boolRunCode = false;
-}
+// var boolRunCode = true;
+// var num1;
+// var cleanInput;
+// var myArray = [];
+//
+// while (boolRunCode === true) {
+//   num1 = getInput();
+//   cleanInput = checkInput(num1); // NOTE: num1 is the Argument--the actual value that's being passed to the function
+//
+//   while (cleanInput !== true) {
+//     num1 = getInput();
+//     cleanInput = checkInput(num1);
+//   }
+//   myArray = initializeArray(num1);
+//   alert("MY ARRAY:" + myArray);
+//   boolRunCode = false;
+// }
