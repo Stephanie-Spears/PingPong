3c179bfda4cc7592b901818cf84acaeddf22cc56
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-20 18:19:07 -0800

    clean up javascript page

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,81 +1,42 @@
 var getInput = function() {
-  var getNumber1 = parseInt(prompt("Enter finishing point number: "));
-  return [getNumber1];
+  num1 = prompt("How high do you want to go?");
+  return num1;
 };
 
-var userInputArray = getInput();
-var num1= userInputArray[0];
-var myArray = [];
-
-
-var displayNum = function (num1) {
-  for (var i = 0; i < num1; i++) {
-    myArray[i] = [i];
-    alert("inside for loop: " + myArray[i]);
+var checkInput = function() {
+  if (isNaN(num1)) {
+    alert("Must input numbers only");
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+};
+// NOTE: DO I NEED TO RENAME THE PASSED IN ARGUMENTS? OR PARAMETERS, I FORGET WHICH IS WHICH
+var initializeArray = function (endNum) {
+  for (var i = 0; i < endNum; i++) {
+    myArray[i] = [i]; // NOTE: is myArray global? why doesn't it have to be passed in?
+    alert("initializing array: " + myArray[i] + "[" + i + "]");
   }
   return myArray;
 };
 
-displayNum(num1);
 
-alert("outside loop: " + myArray);
-// var displayNum = [
-//   function (num1) {for (var i = 0; i < num1; i++) {
-//     var myArray = [i];
-//     return myArray;}
-//   },
-//   function (num1, num2) {return num1 - num2;},
-//   function (num1, num2) {return num1 / num2;},
-//   function (num1, num2) {return num1 * num2;},
-// ];
+var boolRunCode = true;
+var num1;
+var cleanInput;
+var myArray = [];
 
-// input allows for decimals (ie. 1.2 is accepted. Specify non-floating point? setPrecision funct?)
-// Should make it immediately check input and only store the int in the array if it's valid...return false immediately, so user doesn't have to go through the motions of finishing input if the first num is wrong
-function checkInput(userInputArray) {
-  for (var i = 0; i < userInputArray.length; i++) {
-    if (isNaN(userInputArray[i])) {
-      alert("user input of '" + userInputArray[i] + "[" + i + "]" + "' is Not a Number");
-      return false;
-    }
-    else if (i === (userInputArray.length - 1)) {
-      return true;
-    }
+while (boolRunCode === true) {
+  num1 = getInput();
+  cleanInput = checkInput(num1);
+
+  while (cleanInput !== true) {
+    num1 = getInput();
+    cleanInput = checkInput(num1);
   }
+  myArray = initializeArray(num1);
+  alert("MY ARRAY:" + myArray);
+  boolRunCode = false;
 }
-
-var cleanInput = checkInput(userInputArray);
-
-while (cleanInput === false) {
-  alert("invalid choice. TRY AGAIN!");
-  var userInputArray = getInput();
-  var num1= userInputArray[0];
-  var cleanInput = checkInput(userInputArray);
-}
-if(cleanInput === true) {
-  alert("User input has been validated");
-  alert("cleanInput Array: " + myArray);
-}
-
-// var displayCalcOperation = function (num1, num2) {
-//   var numValue;
-//   alert(num1 + calcSymb[userChoice-1] + num2 + " = " + (numValue = calculateNum[userChoice-1](num1, num2)));
-// };
-//
-// displayCalcOperation(num1, num2);
-
-$(function() {
-    $( "#button1" ).click(function() {
-        $( "#item1" ).toggle();
-    });
-});
-
-$(function() {
-    $( "#button2" ).click(function() {
-        $( "#item2" ).toggle();
-    });
-});
-$(function() {
-    $( "#button3" ).click(function() {
-        $( "#item3" ).toggle();
-    });
-});
