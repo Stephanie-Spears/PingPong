074403ee10bd286689ecf7fe6a09185a20c077ef
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-22 20:59:02 -0800

    reformat functions in a new file--cleans up

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,36 +1,34 @@
 // BACK-END
 // NOTE: encapsulate the vars
 
-var myArray = [];
-var num1;
-var mutatedArray = [];
-
 function getFormInput(num1) {
+  // var myArray = [];
+  // var mutatedArray = [];
 
   var boolRunCode = true;
   var cleanInput;
 
  while (boolRunCode === true) {
-   num1 = getInput();
+  //  num1 = getInput();
    cleanInput = checkInput(num1);
 
    while (cleanInput !== true) {
-     num1 = getInput();
+    //  num1 = getInput();
      cleanInput = checkInput(num1);
    }
    myArray = initializeArray(num1);
    alert("MY ARRAY:" + myArray);
-   mutateArrayFunction(myArray);
+   mutatedArray = mutateArrayFunction(myArray);
    alert("MUTATED ARRAY: " + mutatedArray);
 
    boolRunCode = false;
  }
 }
 
-var getInput = function() {
-  num1 = prompt("How high do you want to go?");
- return num1;
-};
+// var getInput = function() {
+//   // num1 = prompt("How high do you want to go?");
+//  return num1;
+// };
 var checkInput = function(num1) { // NOTE: check input is unnecessary with "required" and added min-max values inside html submit
  if (isNaN(num1)) {
    alert("Must input numbers only");
@@ -42,13 +40,16 @@ var checkInput = function(num1) { // NOTE: check input is unnecessary with "requ
  }
 };
 var initializeArray = function (num1) {
+  var myArray = [];
  for (var i = 0; i < num1; i++) {
    myArray.push(myArray[i]);
    myArray[i] = i + 1;
  }
  return myArray;
 };
+
 var mutateArrayFunction = function(myArray) {
+  var mutatedArray = myArray;
   for (var i = 0; i < myArray.length; i++) {
     if (myArray[i] % 15 === 0) {
       mutatedArray.push("ping-pong");
@@ -62,6 +63,7 @@ var mutateArrayFunction = function(myArray) {
     else {
       mutatedArray.push(myArray[i]);
     }
+    return mutatedArray;
   }
 };
 
@@ -70,27 +72,20 @@ $(document).ready(function(){
   $("#playButton").click(function(){
     $("html, body").animate({scrollTop: $("#gameContainer").offset().top}, "slow");
   });
-  $("#submitButton").click(function(event){
-    event.preventDefault();
-    num1 = parseInt($("#userInput").val());
-    getFormInput(num1);
-    for (var i = 0; i < num1; i++){
-      $("#outputList").append("<li>" + mutatedArray[i] + "</li>");
-    }
-  });
-
   $("form").submit(function(event){ //event used for prevent default
     event.preventDefault();
-    num1 = parseInt($("#userInput").val());
-    getFormInput(num1);
+    // var num1 = getFormInput($("#userInput").val());
+    var num1 = parseInt($("#userInput").val());
+
+
+
+  getFormInput(num1);
     // $("#output").text(mutatedArray);
     for (var i = 0; i < num1; i++){
       $("#outputList").append("<li>" + mutatedArray[i] + "</li>");
     }
   });
   $("#resetButton").click(function(){
-    myArray = [];
-    mutatedArray = [];
-    $("#outputList").remove();
+    $("#outputList").reset(); //changed from remvoe to reset
   });
 });
---
+++ js/myScripts2.js
@@ -0,0 +1,93 @@
+// BACK-END
+
+function myControlStructure(){
+  var boolRunCode = true;
+  var numValidate;
+  var myArray = [];
+  var myMutatedArray = [];
+
+  var num1 = parseInt($("#userInput").val()); 
+  while(boolRunCode === true)
+  {
+    numValidate = checkInput(num1);
+
+    while(numValidate !== true)
+    {
+      num1 = parseInt($("#userInput").val());
+      numValidate = checkInput(num1);
+    }
+
+    myArray = initializeArray(num1);
+    myMutatedArray = mutateArray(myArray);
+    displayArray(myMutatedArray);
+
+    alert("MY ARRAY: " + myArray);
+    alert("MY MUTATED ARRAY: " + myMutatedArray);
+    boolRunCode = false;
+  }
+  return (myMutatedArray);
+}
+
+function checkInput(num1){
+  if (isNaN(num1)){
+    alert("Must Input Numbers Only");
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+function initializeArray(num1){
+  var returnMyArray = [];
+  for (var i = 0; i < num1; i++){
+    returnMyArray.push(returnMyArray[i]);
+    returnMyArray[i] = i + 1;
+  }
+  return (returnMyArray);
+}
+
+function mutateArray(myArray){
+  var returnMutatedArray = [];
+  for (var i = 0; i < myArray.length; i++){
+    if (myArray[i] % 15 === 0){
+      returnMutatedArray.push("ping-pong");
+    }
+    else if (myArray[i] % 5 === 0){
+      returnMutatedArray.push("pong");
+    }
+    else if (myArray[i] % 3 === 0){
+      returnMutatedArray.push("ping");
+    }
+    else{
+      returnMutatedArray.push(myArray[i]);
+    }
+  }
+  return (returnMutatedArray);
+}
+
+function displayArray(myMutatedArray){
+  for(var i = 0; i < myMutatedArray.length; i++){
+    $("#outputList").append("<li>" + myMutatedArray[i] + "</li>");
+  }
+}
+
+
+$(document).ready(function(){
+  $("#playButton").click(function(){
+    $("html, body").animate({
+      scrollTop: $("#gameContainer").offset().top
+    }, "slow");
+  });
+  $("form").submit(function(event){
+    event.preventDefault();
+    // var num1 = parseInt($("#userInput").val());
+    // myControlStructure(num1);
+    myControlStructure();
+  });
+
+  $("#resetButton").click(function(){
+    $("#outputList").reset(); //changed from remove to reset
+  });
+
+});
